a630080c7c742f64c31ebee19a35f57616024e81
Anmol Sen <105110161+Anmo1Sen@users.noreply.github.com>
2022-06-04 13:22:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+    d = new Date(); //object of date()
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+  
+    hour = document.getElementById("hour_hand");
+    minute = document.getElementById("minute_hand");
+    second = document.getElementById("second_hand");
+  
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+  }, 1000);
